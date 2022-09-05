ba1ecb84417076ccf9298904fe0a96019b0730ff
Pius Ashogbon <Pius1ash@gmail.com>
2022-09-05 20:24:21 +0100

    Add project files.

---
+++ Object Oriented Programming/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Object_Oriented_Programming
+{
+    class Program 
+    {
+        
+
+        static void Main(string[] args)
+        {
+            car myObject = new car("Venza", "Testing", 4004);
+
+           
+            myObject.color = "blue";  //This is setting
+            myObject.sem = 300;
+            car modelName = new car("Venza", "Testing", 4000);
+            Console.WriteLine(myObject.color);
+          myObject.full();
+            Console.WriteLine(modelName.model);
+            Console.WriteLine(myObject.model);
+            Console.WriteLine($"{modelName.type}, { modelName.type}, { modelName.color},{ modelName.sem}");
+            Console.WriteLine(modelName.type);
+            Console.WriteLine(modelName.color);
+        }
+    }
+}
---
+++ Object Oriented Programming/car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Object_Oriented_Programming
+{
+   public class car 
+    {
+       public string color = "red"; //This is a field
+        public int maxSpeed = 200;  //This is a field
+        public int sem;  //Blank field
+        public string model; //this is a blank field
+        public string type;
+        public int year; 
+
+        public car(string modelName, string typeName, int modelYear)
+        {
+            // model = "Venza";
+            model = modelName;
+            type = typeName;
+            year = modelYear;
+            
+
+        }
+
+
+        public void full()
+        {
+            Console.WriteLine("The car is going as fast as it can");
+        }
+    }
+
+
+}
